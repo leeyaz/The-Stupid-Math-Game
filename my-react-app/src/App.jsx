1738176df61652bc9f9375826e175cb4ff91a191
leeyaz <leeya.zhang5976@gmail.com>
2026-06-20 17:58:18 -0700

    updated Instructions and Info-Bracket
    actually wrote the instructions. put a link in the link to https://mathjs.org/docs/expressions/syntax.html in info-bracket.

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -42,14 +42,19 @@ function App() {
             <p>Continuing: {continuing}</p>
             <p>Target: {target}</p>
             <p>Available Operations: +, -, *, /, (, ), ^ (power), % (modulus or percentage), ! (factorial), sin, cos, tan, sec, csc, cot,
-                floor, pi, e, ...and more. Check https://mathjs.org/docs/expressions/syntax.html for more information.
+                floor, pi, e, ...and more. Check <a href = "https://mathjs.org/docs/expressions/syntax.html">this website</a> for more information.
             </p>
         </div>
 
         {/*on the left bottom, includes valid operations*/}
         <div className="instructions">
             <h6><b>INSTRUCTIONS TO THE GAME</b></h6>
-            <p>yap yap yap</p>
+            <p>
+                Your expression MUST begin with the Start number once, today's is {start}. After {start}, you may only use the Continuing number, 
+                which is {continuing} today, which you may use any number of times. Your goal is to bring the operation to equal to the target number, 
+                that number is {target} today. Your score is counted by the length of your expression, your goal is to find the shortest expression. 
+                All number combinations are possible. See above for available operations, numbers randomize from 0 to 100 every day.
+            </p>
         </div>
 
         {/*on the right top, scrollable*/}
