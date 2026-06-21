e9a4064f46148742ba254afd4bfe82fdbfc24205
Mexicanlove <63421436+Mexicanlove@users.noreply.github.com>
2026-06-20 19:24:21 -0700

    Title??

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -28,9 +28,13 @@ function App() {
     };
 
     return (
-        <div className = "container">
+        <div className="container">
             <div className="title">
-                <p>title placeholder</p>
+                <h1>
+                    <b>STUPID MATH</b>
+                    <span style={{ fontSize: "10px" }}> the </span>
+                    <b>GAME</b>™
+                </h1>
             </div>
 
             {/*at the centre*/}
@@ -45,20 +49,33 @@ function App() {
                 <p>Start: {start}</p>
                 <p>Continuing: {continuing}</p>
                 <p>Target: {target}</p>
-                <p>Available Operations: +, -, *, /, (, ), ^ (power), % (modulus or percentage), ! (factorial), sin, cos, tan, sec, csc, cot,
-                    floor, pi, e, ...and more. Check out <a href = "https://mathjs.org/docs/expressions/syntax.html">this website</a> for more information.
+                <p>
+                    Available Operations: +, -, *, /, (, ), ^ (power), %
+                    (modulus or percentage), ! (factorial), sin, cos, tan, sec,
+                    csc, cot, floor, pi, e, ...and more. Check out{" "}
+                    <a href="https://mathjs.org/docs/expressions/syntax.html">
+                        this website
+                    </a>{" "}
+                    for more information.
                 </p>
             </div>
 
             {/*on the left bottom, includes valid operations*/}
             <div className="instructions">
-                <h6><b>INSTRUCTIONS TO THE GAME</b></h6>
+                <h6>
+                    <b>INSTRUCTIONS TO THE GAME</b>
+                </h6>
                 <p>
-                    Your expression MUST begin with the Start number once, today's is {start}. After {start}, you may only use the Continuing number, 
-                    which is {continuing} today, which you may use any number of times. Your goal is to bring the operation to equal to the target number, 
-                    that number is {target} today. Your score is counted by the length of your expression, your goal is to find the shortest expression. 
-                    All number combinations are possible. See above for available operations, numbers randomize from 0 to 100 every day. Have fun playing!! 
-                    :)
+                    Your expression MUST begin with the Start number once,
+                    today's is {start}. After {start}, you may only use the
+                    Continuing number, which is {continuing} today, which you
+                    may use any number of times. Your goal is to bring the
+                    operation to equal to the target number, that number is{" "}
+                    {target} today. Your score is counted by the length of your
+                    expression, your goal is to find the shortest expression.
+                    All number combinations are possible. See above for
+                    available operations, numbers randomize from 0 to 100 every
+                    day. Have fun playing!! :)
                 </p>
             </div>
 
