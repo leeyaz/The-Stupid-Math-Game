5cd40199cbe6be7ddc2e2f691394abea04942bc0
Mexicanlove <63421436+Mexicanlove@users.noreply.github.com>
2026-06-20 18:58:04 -0700

    Daily numbers are now based on seeded rng generator (Mulberry32 PRNG) and will be the same across ppl on a given day ?|: u )

--- my-react-app/src/utils/DailyNumbers.js
+++ my-react-app/src/utils/DailyNumbers.js
@@ -1,18 +1,34 @@
+function mulberry32(a) {
+    return function () {
+        let t = (a += 0x6d2b79f5);
+        t = Math.imul(t ^ (t >>> 15), t | 1);
+        t ^= t + Math.imul(t ^ (t >>> 7), t | 61);
+        return ((t ^ (t >>> 14)) >>> 0) / 4294967296;
+    };
+}
+
 export function GetDailyNumbers() {
-    const today = new Date().toDateString();
-    const stored = localStorage.getItem("dailyNumbers");
+    const date = new Date();
+    const dateInteger =
+        date.getDate() + date.getMonth() * 100 + date.getFullYear() * 10000;
+    const rand = mulberry32(dateInteger);
+
+    /*const stored = localStorage.getItem("dailyNumbers");
 
     if (stored) {
         const { date, numbers } = JSON.parse(stored);
         if (date === today) return numbers;
-    }
+    }*/
 
     const numbers = {
-        start: Math.floor(Math.random() * 101) - 0,
-        continuing: Math.floor(Math.random() * 101) - 0,
-        target: Math.floor(Math.random() * 101) - 0,
+        start: Math.floor(rand() * 101) - 0,
+        continuing: Math.floor(rand() * 101) - 0,
+        target: Math.floor(rand() * 101) - 0,
     };
 
-    localStorage.setItem("dailyNumbers", JSON.stringify({ date : today, numbers }));
+    /* localStorage.setItem(
+        "dailyNumbers",
+        JSON.stringify({ date: today, numbers }),
+    );*/
     return numbers;
 }
