2a2eeaad163b20e88bc7ee29506487e9b8c374cc
leeyaz <leeya.zhang5976@gmail.com>
2026-06-20 18:07:37 -0700

    added title placeholder
    also tabbed some divs in App.jsx

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -29,43 +29,48 @@ function App() {
 
     return (
         <div className = "container">
-        {/*at the centre*/}
-        <div className="calculator">
-            <Input onSubmit={handleSubmit}></Input>
-            <Display value={output} valid={valid}></Display>
-        </div>
+            <div className="title">
+                <p>title placeholder</p>
+            </div>
 
-        {/*on the left top, tells user what the starting, continuing and target numbers are*/}
-        <div className="info-bracket">
-            <h5>TODAY'S NUMBERS</h5>
-            <p>Start: {start}</p>
-            <p>Continuing: {continuing}</p>
-            <p>Target: {target}</p>
-            <p>Available Operations: +, -, *, /, (, ), ^ (power), % (modulus or percentage), ! (factorial), sin, cos, tan, sec, csc, cot,
-                floor, pi, e, ...and more. Check <a href = "https://mathjs.org/docs/expressions/syntax.html">this website</a> for more information.
-            </p>
-        </div>
+            {/*at the centre*/}
+            <div className="calculator">
+                <Input onSubmit={handleSubmit}></Input>
+                <Display value={output} valid={valid}></Display>
+            </div>
 
-        {/*on the left bottom, includes valid operations*/}
-        <div className="instructions">
-            <h6><b>INSTRUCTIONS TO THE GAME</b></h6>
-            <p>
-                Your expression MUST begin with the Start number once, today's is {start}. After {start}, you may only use the Continuing number, 
-                which is {continuing} today, which you may use any number of times. Your goal is to bring the operation to equal to the target number, 
-                that number is {target} today. Your score is counted by the length of your expression, your goal is to find the shortest expression. 
-                All number combinations are possible. See above for available operations, numbers randomize from 0 to 100 every day.
-            </p>
-        </div>
+            {/*on the left top, tells user what the starting, continuing and target numbers are*/}
+            <div className="info-bracket">
+                <h5>TODAY'S NUMBERS</h5>
+                <p>Start: {start}</p>
+                <p>Continuing: {continuing}</p>
+                <p>Target: {target}</p>
+                <p>Available Operations: +, -, *, /, (, ), ^ (power), % (modulus or percentage), ! (factorial), sin, cos, tan, sec, csc, cot,
+                    floor, pi, e, ...and more. Check out <a href = "https://mathjs.org/docs/expressions/syntax.html">this website</a> for more information.
+                </p>
+            </div>
 
-        {/*on the right top, scrollable*/}
-        <div className="scoreboard">
-            <p>scoreboard placeholder</p>
-        </div>
+            {/*on the left bottom, includes valid operations*/}
+            <div className="instructions">
+                <h6><b>INSTRUCTIONS TO THE GAME</b></h6>
+                <p>
+                    Your expression MUST begin with the Start number once, today's is {start}. After {start}, you may only use the Continuing number, 
+                    which is {continuing} today, which you may use any number of times. Your goal is to bring the operation to equal to the target number, 
+                    that number is {target} today. Your score is counted by the length of your expression, your goal is to find the shortest expression. 
+                    All number combinations are possible. See above for available operations, numbers randomize from 0 to 100 every day. Have fun playing!! 
+                    :)
+                </p>
+            </div>
 
-        {/*on the right bottom*/}
-        <div className="nameInput">
-            <p>nameInput placeholder</p>
-        </div>
+            {/*on the right top, scrollable*/}
+            <div className="scoreboard">
+                <p>scoreboard placeholder</p>
+            </div>
+
+            {/*on the right bottom*/}
+            <div className="nameInput">
+                <p>nameInput placeholder</p>
+            </div>
         </div>
     );
 }
