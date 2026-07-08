c3daa176983cde4e0383992b4f92a68140f87cc6
Mexicanlove <63421436+Mexicanlove@users.noreply.github.com>
2026-07-08 03:51:21 -0700

    Input now handles parentheses - automatically places ")" if u type "(" - if you backspace "(" when there is a ")" next to it, it will delete both

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,4 +1,4 @@
-import { Fragment, useState } from "react";
+import { Fragment, useRef, useState } from "react";
 import { evaluate, ParseExpression } from "../utils/ParseExpressionUtil.js";
 import { GetDailyNumbers } from "../utils/DailyNumbers.js";
 import { Button } from "react-bootstrap";
@@ -7,6 +7,53 @@ function Input({ onSubmit, showDisplay }) {
     const { start, continuing, target } = GetDailyNumbers();
     const [currInput, setCurrInput] = useState("");
     const [currResult, setCurrResult] = useState("~");
+    const taRef = useRef(null);
+
+    const handleKeyDown = (e) => {
+        const key = e.key;
+        const start = e.target.selectionStart;
+        const end = e.target.selectionEnd;
+        const selected = currInput.substring(start, end);
+        if (key == "(") {
+            e.preventDefault();
+            // == Handle Parenthesis == \\
+            const newValue =
+                currInput.substring(0, start) +
+                "(" +
+                selected +
+                ")" +
+                currInput.substring(end);
+            setCurrInput(newValue);
+            e.target.style.height = "auto";
+            e.target.style.height = e.target.scrollHeight + "px";
+
+            try {
+                setCurrResult(evaluate(newValue));
+            } catch (e) {
+                setCurrResult("~");
+            }
+
+            setTimeout(() => {
+                const cursorPos = start + 1;
+                taRef.current.focus();
+                taRef.current.setSelectionRange(cursorPos, cursorPos);
+            }, 0);
+        } else if (key == "Backspace") {
+            if (start === end && start > 0) {
+                const charBefore = currInput[start - 1];
+                const charAfter = currInput[start];
+                if (charBefore == "(" && charAfter == ")") {
+                    e.preventDefault();
+
+                    const newValue =
+                        currInput.substring(0, start - 1) +
+                        currInput.substring(start + 1);
+
+                    setCurrInput(newValue);
+                }
+            }
+        }
+    };
 
     const handleInputChange = (e) => {
         if (e.target.value.length == 0) {
@@ -27,6 +74,9 @@ function Input({ onSubmit, showDisplay }) {
         <div className="d-flex flex-column">
             <textarea
                 type="text"
+                id="math-input"
+                ref={taRef}
+                onKeyDown={handleKeyDown}
                 className="math-input text-center rounded-3"
                 placeholder="Enter the MATH EXPRESSION"
                 value={currInput}
