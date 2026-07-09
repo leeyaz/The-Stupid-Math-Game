e7bc11b3f70e998287e327b9a2292b206605e5b8
Mexicanlove <63421436+Mexicanlove@users.noreply.github.com>
2026-07-08 17:02:07 -0700

    Fixed parentheses on mobile - also made it keep the selection when you surround it with parentheses

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,59 +1,14 @@
-import { Fragment, useRef, useState } from "react";
+import { Fragment, useEffect, useRef, useState } from "react";
 import { evaluate, ParseExpression } from "../utils/ParseExpressionUtil.js";
 import { GetDailyNumbers } from "../utils/DailyNumbers.js";
 import { Button } from "react-bootstrap";
+import { flushSync } from "react-dom";
 
 function Input({ onSubmit, showDisplay }) {
     const { start, continuing, target } = GetDailyNumbers();
     const [currInput, setCurrInput] = useState("");
     const [currResult, setCurrResult] = useState("~");
     const taRef = useRef(null);
-
-    const handleKeyDown = (e) => {
-        const key = e.key;
-        const start = e.target.selectionStart;
-        const end = e.target.selectionEnd;
-        const selected = currInput.substring(start, end);
-        if (key == "(") {
-            e.preventDefault();
-            // == Handle Parenthesis == \\
-            const newValue =
-                currInput.substring(0, start) +
-                "(" +
-                selected +
-                ")" +
-                currInput.substring(end);
-            setCurrInput(newValue);
-            e.target.style.height = "auto";
-            e.target.style.height = e.target.scrollHeight + "px";
-
-            try {
-                setCurrResult(evaluate(newValue));
-            } catch (e) {
-                setCurrResult("~");
-            }
-
-            setTimeout(() => {
-                const cursorPos = start + 1;
-                taRef.current.focus();
-                taRef.current.setSelectionRange(cursorPos, cursorPos);
-            }, 0);
-        } else if (key == "Backspace") {
-            if (start === end && start > 0) {
-                const charBefore = currInput[start - 1];
-                const charAfter = currInput[start];
-                if (charBefore == "(" && charAfter == ")") {
-                    e.preventDefault();
-
-                    const newValue =
-                        currInput.substring(0, start - 1) +
-                        currInput.substring(start + 1);
-
-                    setCurrInput(newValue);
-                }
-            }
-        }
-    };
 
     const handleInputChange = (e) => {
         if (e.target.value.length == 0) {
@@ -70,13 +25,68 @@ function Input({ onSubmit, showDisplay }) {
         }
     };
 
+    useEffect(() => {
+        const ta = taRef.current;
+        if (!ta) return;
+
+        const onBeforeInput = (e) => {
+            const start = ta.selectionStart;
+            const end = ta.selectionEnd;
+            if (e.data?.length === 1 && e.data === "(") {
+                e.preventDefault();
+                const selected = ta.value.slice(start, end);
+                console.log(
+                    start,
+                    end,
+                    ta.value.slice(0, start),
+                    selected,
+                    ta.value.slice(end, ta.value.length),
+                );
+                ta.value =
+                    ta.value.slice(0, start) +
+                    "(" +
+                    selected +
+                    ")" +
+                    ta.value.slice(end, ta.value.length);
+                ta.selectionStart = start + 1;
+                ta.selectionEnd = end + 1;
+
+                setCurrInput(ta.value);
+            } else if (
+                start == end &&
+                ta.value[end - 1] == "(" &&
+                ta.value[end] == ")" &&
+                e.inputType == "deleteContentBackward"
+            ) {
+                e.preventDefault();
+                ta.value =
+                    ta.value.slice(0, start - 1) +
+                    ta.value.substring(start + 1);
+                ta.selectionStart = start - 1;
+                ta.selectionEnd = start - 1;
+                setCurrInput(ta.value);
+            }
+            try {
+                setCurrResult(evaluate(ta.value));
+            } catch (e) {
+                setCurrResult("~");
+            }
+        };
+
+        ta.addEventListener("beforeinput", onBeforeInput);
+        return () => {
+            ta.removeEventListener("beforeinput", onBeforeInput);
+        };
+    }, []);
+
     return (
         <div className="d-flex flex-column">
             <textarea
                 type="text"
                 id="math-input"
                 ref={taRef}
-                onKeyDown={handleKeyDown}
+                //  onBeforeInput={handleBeforeInput}
+                //onKeyDown={handleKeyDown}
                 className="math-input text-center rounded-3"
                 placeholder="Enter the MATH EXPRESSION"
                 value={currInput}
